fb6c5c055a4a088b946713b6a63d816968422e89
Michael <ceo.wonu@gmail.com>
2012-02-23 18:01:52 -0600

    Initial Commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MysneaksApp::Application.config.session_store :cookie_store, :key => '_mysneaks_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MysneaksApp::Application.config.session_store :active_record_store
